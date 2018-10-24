8a8ef688b9528f19326341adb059abb4ea4d1601
YANLIN LIU <leo@YANLINs-MacBook-Pro.local>
2018-10-24 22:10:58 +1100

    content rewrite

---
+++ app/nav.js
@@ -0,0 +1,18 @@
+// Define the `my-site` module
+var my_site = angular.module('my_site', []);
+
+// Define the `PhoneListController` controller on the `phonecatApp` module
+my_site.controller('PhoneListController', function PhoneListController($scope) {
+  $scope.phones = [
+    {
+      name: 'Nexus S',
+      snippet: 'Fast just got faster with Nexus S.'
+    }, {
+      name: 'Motorola XOOM™ with Wi-Fi',
+      snippet: 'The Next, Next Generation tablet.'
+    }, {
+      name: 'MOTOROLA XOOM™',
+      snippet: 'The Next, Next Generation tablet.'
+    }
+  ];
+});
