cc3938ecbd529426289b325ec1e9a6c418c8934f
Yanlin Liu <30500146+Louleo@users.noreply.github.com>
2019-02-25 21:30:39 +1100

    Under development

---
+++ mortgagecal.js
@@ -0,0 +1,14 @@
+function repaymentCal() {
+  var loan_amount = $('#loan_amount').val();
+  var fixed_rate = $('fixed_rate').val()/100;
+  var fixed_loan_period = $('#fixed_loan_period').val();
+  var variable_interest_rate = $('#variable_interest_rate').val()/100;
+  var variable_loan_period = $('#variable_loan_period').val();
+
+
+
+}
+
+function periodCal() {
+
+}
